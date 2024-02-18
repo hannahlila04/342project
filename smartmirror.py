be9ed2310c17a9fbfc2bd8f27a8f7d98869cc087
Anoop Rehman <93463679+anoop-rehman@users.noreply.github.com>
2024-02-18 12:49:18 -0500

    removed testings

--- smartmirror.py
+++ smartmirror.py
@@ -468,17 +468,14 @@ if __name__ == '__main__':
             ser.open()
         print("Serial port opened successfully.")
        
-        i = 0
+        # i = 0
 
         while True:
-            # if ser.in_waiting > 0:
-            if True:
-    
-                if True:
-                    # received_data = ser.readline().decode().strip()  # Read and decode the received data
-                    received_data = "p 100 " +  str(i) # test value
-                    print("Received data:", received_data)
-                    i += 1
+            if ser.in_waiting > 0:
+                received_data = ser.readline().decode().strip()  # Read and decode the received data
+                # received_data = "p 100 " +  str(i) # test value
+                print("Received data:", received_data)
+                # i += 1
 
             # Parse the received data
             if received_data.startswith('p') and received_data.count(' ') == 1:
